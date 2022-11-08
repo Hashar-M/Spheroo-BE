42fab4a507f9ed0097837756e374fb01ce357ef4
Hameel <hameel@qburst.com>
2022-11-08 16:35:47 +0530

    Implemented CategoryService interface.

---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/category/CategoryService.java
@@ -0,0 +1,12 @@
+package com.qburst.spherooadmin.category;
+
+import org.springframework.data.domain.Page;
+
+public interface CategoryService {
+    void saveCategory(Category category);
+    Category getCategory(Long id);
+    void updateCategoryById(Long categoryId, Category category);
+    void deleteCategory(Long id);
+    Page<Category> getAllCategoriesPaged(int pageNo, int noOfElements);
+    void updateCategoryIconById(Long categoryId, String categoryIconPath);
+}
