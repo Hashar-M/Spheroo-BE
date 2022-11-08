35d1a2a0d27a8c498646bef020ab79aae48c2317
Hameel <hameel@qburst.com>
2022-11-08 16:36:04 +0530

    Implemented CategoryService implementation.

---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/category/CategoryServiceImpl.java
@@ -0,0 +1,49 @@
+package com.qburst.spherooadmin.category;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class CategoryServiceImpl implements CategoryService{
+    private CategoryRepository categoryRepository;
+
+    @Override
+    @Transactional
+    public void saveCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public Category getCategory(Long id) {
+        return categoryRepository.getReferenceById(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateCategoryById(Long categoryId, Category category) {
+        categoryRepository.updateCategoryById(category.getCategoryName(), category.getCategoryDescription(), category.getCategoryIcon(), categoryId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCategory(Long id) {
+       categoryRepository.deleteByCategoryId(id);
+    }
+
+    @Override
+    public Page<Category> getAllCategoriesPaged(int pageNo, int noOfElements) {
+        Pageable pageWithRequiredElements = PageRequest.of(pageNo, noOfElements, Sort.by("categoryId"));
+        return categoryRepository.findAll(pageWithRequiredElements);
+    }
+    @Transactional
+    @Override
+    public void updateCategoryIconById(Long categoryId, String categoryIconPath) {
+        categoryRepository.updateCategoryIconByCategoryId(categoryIconPath, categoryId);
+    }
+}
