35dfc9c8ccc341444745b7800187361e4d5e9686
Hameel <hameel@qburst.com>
2022-11-09 08:37:21 +0530

    Replaced * imports with individual imports

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/category/Category.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/category/Category.java
@@ -6,7 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+
+
 
 @Entity
 @Getter
