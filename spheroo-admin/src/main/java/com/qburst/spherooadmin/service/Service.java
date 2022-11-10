88e9be3b8b9ea7e6406ae82d1e0da7ac33ac3fa9
hashar-01 <hashar@qburst.com>
2022-11-10 18:23:33 +0530

    created model for service and charge, need to change mapping type in both models

---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/service/Service.java
@@ -0,0 +1,54 @@
+package com.qburst.spherooadmin.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.qburst.spherooadmin.category.Category;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+// When hibernate fetches the data these fields are included in the json which can be ignored when we serialize it.
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "service")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long serviceId;
+
+    @Column(name = "service_name", length = 64, nullable = false, unique = true)
+    private String serviceName;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "description", length = 1024)
+    private String description;
+
+    @Column(name="variable_price")
+    private String variablePrice;
+
+    @OneToMany(targetEntity = ServiceCharge.class)
+    private List<ServiceCharge>  serviceChargeList;
+}
---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceCharge.java
@@ -0,0 +1,48 @@
+package com.qburst.spherooadmin.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+// When hibernate fetches the data these fields are included in the json which can be ignored when we serialize it.
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "service_charge")
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceCharge
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "charge_id")
+    private long chargeId;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "service_id")
+    private Service service;
+
+    @Column(name = "service_priority")
+    private String servicePriority;
+
+    @Column(name = "time_frame")
+    private String timeFrame;
+
+    @Column(name = "charge")
+    private double charge;
+
+}
