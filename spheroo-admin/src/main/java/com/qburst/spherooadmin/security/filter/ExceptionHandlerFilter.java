ce1de2c0c6f6a854be481f926c173f5b2d4ea503
Hameel <hameel@qburst.com>
2022-11-15 17:21:38 +0530

    Added ExceptionHandlerFilter for exceptions occuring during authentication and authorization

---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,35 @@
+package com.qburst.spherooadmin.security.filter;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Since Authentication and Authorization occurs before getting into the controller, any exceptions
+ * thrown during, will not be picked up by the global exception handler, so we create a filter for the
+ * request to go through that handles those exceptions.
+ */
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JWTVerificationException e) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("JWT NOT VALID");
+            response.getWriter().flush();
+        } catch (RuntimeException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("BAD REQUEST");
+            response.getWriter().flush();
+        }
+    }
+}
