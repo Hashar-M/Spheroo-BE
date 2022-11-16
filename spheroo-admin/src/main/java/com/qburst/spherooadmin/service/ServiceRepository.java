e24fda5661af12ead963bf0365fd9f4bf3b93775
Hameel <hameel@qburst.com>
2022-11-16 14:16:47 +0530

    Added repository annotation

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceRepository.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceRepository.java
@@ -1,8 +1,11 @@
 package com.qburst.spherooadmin.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Service,Long> {
     /**
      * function for getting list of service_id in which won't have any relation to any category.
