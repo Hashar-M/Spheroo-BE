670a4d47aa7717ebf366842183ed99d0f7969f71
Hameel <hameel@qburst.com>
2022-11-17 10:21:22 +0530

    Added CORS filter for front end to be able to make api calls

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/security/SecurityConfiguration.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.qburst.spherooadmin.security;
 
 import com.qburst.spherooadmin.constants.SecurityConstants;
 import com.qburst.spherooadmin.security.filter.AuthenticationFilter;
+import com.qburst.spherooadmin.security.filter.CorsFilter;
 import com.qburst.spherooadmin.security.filter.ExceptionHandlerFilter;
 import com.qburst.spherooadmin.security.filter.JWTAuthorizationFilter;
 import com.qburst.spherooadmin.security.manager.CustomAuthenticationManager;
@@ -11,6 +12,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * Security configuration class that is used to configure spring security.
@@ -38,9 +45,28 @@ public class SecurityConfiguration {
                 .antMatchers(HttpMethod.POST, SecurityConstants.LOGIN_PATH).permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
                 .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
                 .addFilter(authenticationFilter)
                 .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("PATCH");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
+        corsConfig.setExposedHeaders(Arrays.asList("X-Get-Header"));
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
