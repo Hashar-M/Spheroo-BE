30f41ddeb026bac09630281963296284ca7282cc
Hameel <hameel@qburst.com>
2022-11-18 11:24:09 +0530

    Added comments to controller

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceController.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceController.java
@@ -25,28 +25,56 @@ public class ServiceController {
 
     private ServiceService serviceService;
 
+    /**
+     * Returns a service by id
+     * @param id ID of the service to be returned
+     * @return an Optional Object that expects a service
+     */
     @GetMapping("/id={id}")
     ResponseEntity<Optional<Service>> getById(@PathVariable long id) {
         return new ResponseEntity<>(serviceService.getServiceById(id), HttpStatus.OK);
     }
 
+    /**
+     * Update service by id
+     * @param service Service entity
+     * @param id of the service to update
+     * @return HttpStatus OK if service was successfully updated
+     */
     @PutMapping("/id={id}")
     ResponseEntity<HttpStatus> updateService(@RequestBody Service service, @PathVariable long id) {
         serviceService.updateServiceById(service.getServiceName(), service.getDescription(), service.getVariablePrice(), service.getServiceChargeList(), id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * Deletes a particular service for a category
+     * @param id id of the service to be deleted
+     * @return HttpStatus NO_CONTENT if service was successfully deleted
+     */
     @DeleteMapping("/id={id}")
     ResponseEntity<HttpStatus> deleteService(@PathVariable long id) {
         serviceService.deleteServiceById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * Gets a page of services under a category
+     * @param id of the category
+     * @param page page number
+     * @param noOfElements number of elements to return at a time
+     * @return Page object along with HttpStatus OK
+     */
     @GetMapping("/page={page}&qty={noOfElements}&id={id}")
     ResponseEntity<Page<Service>> getServicesByCategoryId(@PathVariable long id, @PathVariable int page, @PathVariable int noOfElements) {
         return new ResponseEntity<>(serviceService.getServiceByCategoryId(id, page, noOfElements), HttpStatus.OK);
     }
 
+    /**
+     * Gets the name of services under a category
+     * @param id of the category
+     * @return a list of strings containing names of services under a category
+     */
     @GetMapping("/category_id={id}")
     ResponseEntity<List<String>> getServiceNamesByCategoryId(@PathVariable long id) {
         return new ResponseEntity<>(serviceService.getServiceNameByCategoryId(id), HttpStatus.OK);
