893911ecd677e6d16aa6bae91c853c25f55b7e0a
akhilesh.g <akhilesh.g@qburst.com>
2022-11-21 18:55:06 +0530

    edited category model by replacing wildcard imports

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/category/Category.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/category/Category.java
@@ -22,6 +22,12 @@ import javax.persistence.Column;
 import javax.persistence.GenerationType;
 import java.util.List;
 import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 /**
