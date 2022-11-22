b29e91f195348aa96f81a8a69ba81cd8ac89f3f9
Hameel <hameel@qburst.com>
2022-11-22 12:32:55 +0530

    Added Checklist entity

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/checklist/Checklist.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/checklist/Checklist.java
@@ -1,19 +1,22 @@
 package com.qburst.spherooadmin.checklist;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AccessLevel;
+import com.qburst.spherooadmin.service.Service;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
 
+/**
+ * @author Hameel
+ * A Checklist is a list of actions for an agent to do under a service.
+ * A Service can have multiple checklists.
+ * A Checklist can have multiple items under it.
+ */
 @Entity
 @Getter
 @Setter
@@ -23,9 +26,40 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Checklist {
+
+    /**
+     * The id for a Checklist
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "checklist_id", nullable = false)
     private Long checklistId;
 
+    /**
+     * The name for a checklist
+     */
+    @Size(min = 1, max = 64)
+    @Column(name = "checklist_name", nullable = false, unique = true)
+    private String checklistName;
+
+    /**
+     * The description for a checklist
+     */
+    @Column(name = "checklist_description")
+    private String checklistDescription;
+
+    /**
+     * A checklist comes under a service. We build a relation by providing the id of
+     * the service as a foreign key
+     */
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_id")
+    private Service service;
+
+    /**
+     * A checklist can have multiple items under it.
+     */
+    @OneToMany(targetEntity = ChecklistItem.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "checklist_id")
+    private List<ChecklistItem> checklistItem;
 }
