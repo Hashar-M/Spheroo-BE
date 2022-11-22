e72a18da3d1e534e7f90ef4ec9a1bd25daf61185
Hameel <hameel@qburst.com>
2022-11-22 14:30:20 +0530

    Replaced wildcard imports

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/checklist/Checklist.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/checklist/Checklist.java
@@ -6,8 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import java.util.List;
 
