f5dbed83369519a2f228bf4f3ed4120d363cf2cd
Hashar Mahmood <hashar@qburst.com>
2022-11-24 19:13:36 +0530

    added API for getting supplier details as default

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/orderDetails/AssignedOrderRepository.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/orderDetails/AssignedOrderRepository.java
@@ -1,6 +1,9 @@
 package com.qburst.spherooadmin.orderDetails;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AssignedOrderRepository extends JpaRepository<AssignedOrder,Long> {
+    @Query(value = "SELECT COUNT(supplier_id) FROM assigned_order WHERE supplier_id = ?1",nativeQuery = true)
+    int getAssignedOrderCount(long supplierId);
 }
---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/supplier/SupplierToAssignDTO.java
@@ -0,0 +1,16 @@
+package com.qburst.spherooadmin.supplier;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SupplierToAssignDTO {
+    private long supplierId;
+    private String supplierName;
+    private String categoryName;
+    private String serviceName;
+    private int rating;
+    private int tier;
+    private int assignedTickets;
+}
