455b75974ce1ce75792b7ee8b87d519d97368236
Hashar Mahmood <hashar@qburst.com>
2022-11-25 10:48:29 +0530

    added more detailed comment for entity fields

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/orderDetails/Orders.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/orderDetails/Orders.java
@@ -22,7 +22,6 @@ import java.util.List;
 /**
  * Represents the orders entity
  * It will contain order details which are available at the order placing time.
- *
  */
 @Getter
 @Setter
@@ -32,42 +31,61 @@ import java.util.List;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name="orders")
 public class Orders {
-
+    /**
+     * order_id field is the primary key for orders table
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="order_id")
     private long orderId;
-
+    /**
+     * customer_name  field stores the name of order issued customer.
+     */
     @NotNull
     @Column(name="customer_name",nullable = false)
     private String customerName;
-
+    /**
+     * category_id field stores the id of selected category.
+     */
     @NotNull
     @Column(name="category_id",nullable = false)
     private long categoryId;
-
+    /**
+     * service_id field stores the id of selected service.
+     */
     @NotNull
     @Column(name="service_id",nullable = false)
     private long serviceId;
-
+    /**
+     * created_date field stores the order creation date.
+     */
     @NotNull
     @Column(name = "created_date",nullable = false)
     private Date createdDate;
-
+    /**
+     * delivery_from_date stores the customer expecting servicing start date.
+     */
     @Column(name="delivery_from_date")
     private Date deliveryFromDate;
-
+    /**
+     * delivery_to_date stores the customer side last date to service.
+     */
     @Column(name="delivery_to_date")
     private Date deliveryToDate;
-
+    /**
+     * comments field stores the customers comments about order.
+     */
     @Column(name="comments",length = 1024)
     private String comments;
-
+    /**
+     * Zipcode stores the zipcode of customer.
+     */
     @NotNull
     @Column(name = "zip_code",nullable = false)
     private String zipCode;
-
-    /** status may have unassigned, unaccepted, accepted, rejected, closed
+    /**
+     * order_status stores the current order status.
+     * status may have unassigned, unaccepted, accepted, rejected, closed
      * open orders:
      *      unassigned,unaccepted
      * closed orders:
@@ -86,6 +104,9 @@ public class Orders {
     @Formula("(SELECT Service.service_name FROM Service where Service.service_id = service_id)")
     private String serviceName;
 
+    /**
+     * it stores the list of image source path which is uploaded with order.
+     */
     @OneToMany(targetEntity = IssueImages.class, cascade = CascadeType.ALL)
     @JoinColumn(name = "order_id")
     private List<IssueImages> imagesList;
