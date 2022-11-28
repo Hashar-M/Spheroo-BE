4537d2a2b6b0415301657d455ff13bb4d2fc9fc9
Hameel <hameel@qburst.com>
2022-11-28 11:09:59 +0530

    Added comments

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/search/OrderFilter.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/search/OrderFilter.java
@@ -2,7 +2,6 @@ package com.qburst.spherooadmin.search;
 
 import com.qburst.spherooadmin.orderDetails.Orders;
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -10,28 +9,49 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.time.LocalDate;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Hameel
+ * The OrderFilter implements a specification which allows us to build predicates
+ * or criteria to search for Orders in the order table.
+ */
 @Data
 public class OrderFilter implements Specification<Orders> {
 
+    /**
+     * The name of the service to search for
+     */
     private String serviceName;
-    private String zipcode;
+    /**
+     * The zip code of the placed order you want to search for
+     */
+    private String zipCode;
+    /**
+     * The delivery from date of the order
+     */
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    private LocalDate fromDate;
+    private Date fromDate;
+    /**
+     * The delivery to date of the order
+     */
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    private LocalDate toDate;
+    private Date toDate;
 
+    /**
+     * Builds a predicate object by which hibernate can use to build a JPQL
+     * query to select the orders from the database based on some required criteria.
+     */
     @Override
     public Predicate toPredicate(Root<Orders> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
         List<Predicate> predicates = new ArrayList<>();
         if (serviceName != null) {
             predicates.add(criteriaBuilder.like(root.get("serviceName"), "%" + serviceName + "%"));
         }
-        if (zipcode != null) {
-            predicates.add(criteriaBuilder.like(root.get("zipCode"), "%" + zipcode + "%"));
+        if (zipCode != null) {
+            predicates.add(criteriaBuilder.like(root.get("zipCode"), "%" + zipCode + "%"));
         }
         if (fromDate != null) {
             predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("deliveryFromDate"), toDate));
@@ -39,6 +59,6 @@ public class OrderFilter implements Specification<Orders> {
         if (toDate != null){
             predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("deliveryToDate"), fromDate));
         }
-        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
     }
 }
