cbb3074c006f017fe94b4e174aa02ffbf41d8a60
Hameel <hameel@qburst.com>
2022-11-28 12:14:36 +0530

    Removed unused imports

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/category/Category.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/category/Category.java
@@ -2,7 +2,6 @@ package com.qburst.spherooadmin.category;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.qburst.spherooadmin.service.Service;
-import com.qburst.spherooadmin.supplier.Supplier;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,18 +16,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+
 import javax.persistence.Table;
 import java.util.List;
-import javax.persistence.*;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.List;
+
 
 
 /**
