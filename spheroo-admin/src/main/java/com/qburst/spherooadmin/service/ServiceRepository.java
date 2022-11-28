9e774af56874d7bfce6301bae461fd363e8bb5f6
akhilesh.g <akhilesh.g@qburst.com>
2022-11-28 12:36:52 +0530

    removed unused import

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceRepository.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/service/ServiceRepository.java
@@ -1,7 +1,7 @@
 package com.qburst.spherooadmin.service;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
