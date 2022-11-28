cbd59011cfb16aafa6ce8ccf2c2278f056f2f9cc
Hashar Mahmood <hashar@qburst.com>
2022-11-28 18:06:33 +0530

    removed unused imports

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/supplier/SupplierService.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/supplier/SupplierService.java
@@ -1,7 +1,5 @@
 package com.qburst.spherooadmin.supplier;
 
-import org.springframework.data.domain.Page;
-
 import java.util.List;
 import java.util.Optional;
 
--- spheroo-admin/src/main/java/com/qburst/spherooadmin/supplier/SupplierServiceImp.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/supplier/SupplierServiceImp.java
@@ -10,7 +10,6 @@ import com.qburst.spherooadmin.supplieruser.SupplierUsersAddDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
