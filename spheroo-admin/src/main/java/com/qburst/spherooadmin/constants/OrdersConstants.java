5d594d31b50ab21f6f070516c258339c4e270448
Hashar Mahmood <hashar@qburst.com>
2022-12-01 14:44:46 +0530

    added two more constants

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/constants/OrdersConstants.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/constants/OrdersConstants.java
@@ -6,4 +6,6 @@ public class OrdersConstants {
     public static final String RED_FLAG_STARTING = "32 HOURS";
     public static final String OVERDUE_STARTING = "48 HOURS";
     public static final String ESCALATIONS_STARTING= "96 HOURS";
+    public static final int OVERDUE_STARTING_INT = 48;
+    public static final int ESCALATIONS_STARTING_INT= 96;
 }
