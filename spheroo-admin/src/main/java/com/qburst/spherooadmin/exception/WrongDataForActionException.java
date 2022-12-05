196b4faa99f3649b276bff1eedd36d2c1c8ff0a5
Hashar Mahmood <hashar@qburst.com>
2022-12-05 11:19:19 +0530

    Added comment for WrongDataForActionException

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/exception/WrongDataForActionException.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/exception/WrongDataForActionException.java
@@ -1,5 +1,8 @@
 package com.qburst.spherooadmin.exception;
 
+/**
+ * this exception handles exceptions which are occurring by giving wrong data to any Operations/Actions
+ */
 public class WrongDataForActionException extends RuntimeException{
     public WrongDataForActionException(String message) {
         super(message);
