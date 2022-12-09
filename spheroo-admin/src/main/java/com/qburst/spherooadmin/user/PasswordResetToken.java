71298228da3830bb1a98e561e52bcf5ab57fa006
Hameel <hameel@qburst.com>
2022-12-09 16:45:24 +0530

    Added comments

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/user/PasswordResetToken.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/user/PasswordResetToken.java
@@ -13,22 +13,40 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import java.util.Date;
 
+/**
+ * @author Hameel
+ * The PasswordResetToken entity is generated for each user whenever they request a password
+ * reset through the forgot password page.
+ */
 @Entity
 @Getter
 @Setter
 public class PasswordResetToken {
+
+    /**
+     * The id for the reset token in the database table.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    /**
+     * The generated reset token for the user
+     */
     @Column(name = "token", nullable = false)
     private String token;
 
+    /**
+     * The PasswordResetToken has a One-to-One relation with a User entity
+     */
     @OneToOne(targetEntity = Users.class, fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name = "user_id")
     private Users user;
 
+    /**
+     * The expiration date for a token, after the expiration date, the token will no longer be valid.
+     */
     @Column(name = "expiry_date", nullable = false)
     private Date expiryDate;
 }
