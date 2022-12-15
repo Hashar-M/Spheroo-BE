d6d008ce6d7f1df9b6d9cdb482d42b0618b86868
Hashar Mahmood <hashar@qburst.com>
2022-12-15 11:52:06 +0530

    removed unwanted imports

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/orderDetails/OrdersServiceImpl.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/orderDetails/OrdersServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
