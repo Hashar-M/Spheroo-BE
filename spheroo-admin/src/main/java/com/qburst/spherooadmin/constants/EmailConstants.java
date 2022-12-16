935b6e075c5bc88982180f2e73250c6750fedd5a
Luckyseer <hameel12@gmail.com>
2022-12-16 13:13:13 +0300

    Added Email Constants file

---
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/constants/EmailConstants.java
@@ -0,0 +1,6 @@
+package com.qburst.spherooadmin.constants;
+
+public final class EmailConstants {
+    public static final String SUBJECT = "Password reset request";
+    public static final long EXPIRY_TIME = 2L;
+}
