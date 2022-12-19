488b00b2269a4eb97b2c247bf902625e83ea464b
Hameel <Hameel@qburst.com>
2022-12-19 12:33:03 +0300

    Added comments to email methods

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/email/EmailServiceImpl.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/email/EmailServiceImpl.java
@@ -6,12 +6,25 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author Hameel
+ * The implementation of the Email Service interface
+ */
 @Component
 @AllArgsConstructor
 public class EmailServiceImpl implements EmailService{
 
+    /**
+     * All email related tasks are taken care of by the Java Mail Sender
+     */
     private JavaMailSender javaMailSender;
 
+    /**
+     * Sends the forgot password email to the user
+     * @param to The email of the user to send it to
+     * @param text The body of the email
+     * @param token The password reset token
+     */
     @Override
     public void sendForgotPasswordMail(String to, String text, String token) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
