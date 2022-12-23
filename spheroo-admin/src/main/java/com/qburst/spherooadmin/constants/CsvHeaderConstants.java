e32cceb0f852350ad80576599872a87df6489b62
Hashar Mahmood <hashar@qburst.com>
2022-12-23 14:25:27 +0530

    changed CSV headers to capital format

--- spheroo-admin/src/main/java/com/qburst/spherooadmin/constants/CsvHeaderConstants.java
+++ spheroo-admin/src/main/java/com/qburst/spherooadmin/constants/CsvHeaderConstants.java
@@ -1,16 +1,16 @@
 package com.qburst.spherooadmin.constants;
 
 public final class CsvHeaderConstants {
-    public static final String H1="order id";
-    public static final String H2="customer name";
-    public static final String H3="created date";
-    public static final String H4="delivery from_date";
-    public static final String H5="delivery to date";
-    public static final String H6="comments";
-    public static final String H7="zip code";
-    public static final String H8="order status";
-    public static final String H9="category name";
-    public static final String H10="service name";
-    public static final String H11="charge";
-    public static final String H12="assigned supplier";
+    public static final String H1="ORDER ID";
+    public static final String H2="CUSTOMER NAME";
+    public static final String H3="CREATED DATE";
+    public static final String H4="DELIVERY FROM DATE";
+    public static final String H5="DELIVERY TO DATE";
+    public static final String H6="COMMENTS";
+    public static final String H7="ZIP CODE";
+    public static final String H8="ORDER STATUS";
+    public static final String H9="CATEGORY NAME";
+    public static final String H10="SERVICE NAME";
+    public static final String H11="CHARGE";
+    public static final String H12="ASSIGNED SUPPLIER";
 }
